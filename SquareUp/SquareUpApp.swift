3d27a3fca6a6b128b747db003bc81ddd028c14f1
owen-taylor <89555814+owenT8@users.noreply.github.com>
2025-08-23 11:32:23 -0600

    Initial Commit

---
+++ SquareUp/SquareUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareUpApp.swift
+//  SquareUp
+//
+//  Created by Owen  Taylor on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SquareUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SquareUpTests/SquareUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  SquareUpTests.swift
+//  SquareUpTests
+//
+//  Created by Owen  Taylor on 8/23/25.
+//
+
+import Testing
+@testable import SquareUp
+
+struct SquareUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
